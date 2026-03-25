9b1d88b1efe0dcf41d1984b661b170e529b00d06
Owen F. O'Connor <owen.oconnor.2024@mumail.ie>
2026-03-25 21:46:07 +0000

    Added some basic naieve plots showing total checks over time across states.
    Added data related to population, explaining how it was sourced and with an example.

---
+++ data/other_sources/population.R
@@ -0,0 +1,62 @@
+# I believe it may be good to make considerations of populations levels alongside these figures.
+# This will allow us to get permit-per-person figures, a more common scale we can use to compare across states.
+
+# to do this I started as such:
+
+library(tidyverse)
+nics_data <- read_csv("data/nics-firearm-background-checks.csv",
+                      col_types = cols(
+                        month = col_character(),
+                        state = col_character(),
+                        .default = col_integer()
+                      )
+) |>
+  mutate(
+    month = ym(month),
+    year  = year(month)
+  )
+# 
+# max(nics_data$year) # 2023
+# min(nics_data$year) # 1998
+
+
+
+
+# This informed me that in our most complete data the range is from 1998 to 2023
+# I then went to https://fred.stlouisfed.org/series/POPTOTUSA647NWDB (first relevant result via google search)
+# I inputted the range and downloaded the relevant data as a csv.
+
+annual_pop <- read_csv("data/other_sources/POPTOTUSA647NWDB.csv") |>
+  rename(pop = POPTOTUSA647NWDB) |>
+  mutate(year = year(observation_date)) |>
+  select(-c(observation_date))
+head(annual_pop)
+
+nics_joined <- nics_data |>
+  left_join(annual_pop, by = "year")
+head(nics_joined)
+
+
+
+
+# Which enables stuff like this:
+mock_summary <- nics_joined |>
+  group_by(year) |>
+  summarise(
+    total_checks      = sum(totals, na.rm = TRUE),
+    pop               = first(pop)          # or mean(pop) / sum(pop) — see note
+  ) |>
+  mutate(avg_checks_p1000 = total_checks / (pop / 1000))
+
+mock_summary_by_state <- nics_joined |>
+  group_by(year, state) |>
+  summarise(
+    total_checks = sum(totals, na.rm = TRUE),
+    pop          = first(pop),
+    .groups      = "drop"
+  ) |>
+  mutate(avg_checks_p1000 = total_checks / (pop / 1000))
+
+mock_summary_by_state |>
+  ggplot(aes(x = year, y = avg_checks_p1000, colour = state)) +
+  geom_point()
--- state_totals_roc/rough_work.R
+++ state_totals_roc/rough_work.R
@@ -1,7 +1,11 @@
+### Does the rate of change in total firearms background checks over time vary across states?
+  
 library(tidyverse)
 library(lubridate)
+library(tseries)
 
-nics_data <- read_csv("data/nics-firearm-background-checks.csv",
+
+nics_data <- read_csv("data/partial/nics-checks-last-five-years.csv",
                       col_types = cols(
                         month = col_character(),
                         state = col_character(),
@@ -13,7 +17,74 @@ nics_data <- read_csv("data/nics-firearm-background-checks.csv",
     year  = year(month)
   )
 
-# yearly aggregates
-nics_data |>
-  group_by(year) |>
-  summarise(total_checks = sum(totals, na.rm = TRUE))
+# very naive messing around
+state_totals <- nics_data |>
+  select(month, state, totals) |>
+  arrange(state, month)
+
+# 1:10
+state_totals |>
+  filter(state %in% unique(state_totals$state)[1:10]) |>
+  ggplot(aes(x = month, y = totals)) +
+  geom_line(colour = "blue") +
+  geom_smooth(method = "loess", se = FALSE,
+              colour = "red") +
+  facet_wrap(~state, scales = "free_y", ncol = 2) +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+  theme_bw()
+
+# 11:20
+state_totals |>
+  filter(state %in% unique(state_totals$state)[11:20]) |>
+  ggplot(aes(x = month, y = totals)) +
+  geom_line(colour = "blue") +
+  geom_smooth(method = "loess", se = FALSE,
+              colour = "red") +
+  facet_wrap(~state, scales = "free_y", ncol = 2) +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+  theme_bw()
+
+# 21:30
+state_totals |>
+  filter(state %in% unique(state_totals$state)[21:30]) |>
+  ggplot(aes(x = month, y = totals)) +
+  geom_line(colour = "blue") +
+  geom_smooth(method = "loess", se = FALSE,
+              colour = "red") +
+  facet_wrap(~state, scales = "free_y", ncol = 2) +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+  theme_bw()
+
+# 31:40
+state_totals |>
+  filter(state %in% unique(state_totals$state)[31:40]) |>
+  ggplot(aes(x = month, y = totals)) +
+  geom_line(colour = "blue") +
+  geom_smooth(method = "loess", se = FALSE,
+              colour = "red") +
+  facet_wrap(~state, scales = "free_y", ncol = 2) +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+  theme_bw()
+
+# 41:50
+state_totals |>
+  filter(state %in% unique(state_totals$state)[41:50]) |>
+  ggplot(aes(x = month, y = totals)) +
+  geom_line(colour = "blue") +
+  geom_smooth(method = "loess", se = FALSE,
+              colour = "red") +
+  facet_wrap(~state, scales = "free_y", ncol = 2) +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+  theme_bw()
+
+# 51:55
+state_totals |>
+  filter(state %in% unique(state_totals$state)[51:55]) |>
+  ggplot(aes(x = month, y = totals)) +
+  geom_line(colour = "blue") +
+  geom_smooth(method = "loess", se = FALSE,
+              colour = "red") +
+  facet_wrap(~state, scales = "free_y", ncol = 1) +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+  theme_bw()
+
